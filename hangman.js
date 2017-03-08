c80e12231c2db53427f291b80fd21b610aef51fd
Ryan Wilkinson <ryanw@Ryans-MacBook-Pro.local>
2017-03-08 10:19:13 -0500

    randomly chooses a word from the array and then converts the letters into dashes

--- hangman.js
+++ hangman.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-	var hiddenWord = '';
-	var hiddenWordArray [];
+	var underscore = '';
+	var word = pickAWord();
 	var livesLeft = 6;
 
-	});	
 
+
+// array of random words for the game
+var overwatchCharacters = ['Genji', 'McCree', 'Pharah', 'Reaper', 'Sombra', 'Tracer', 'Bastion', 'Hanzo', 'Junkrat', 'Mei', 'Torbjorn', 'Widowmaker', 'DVA', 'Orisa', 'Reinhardt', 'Roadhog', 'Winston', 'Zarya', 'Ana', 'Lucio', 'Mercy', 'Symmetra', 'Zenyatta'];
+
+var randomWord = overwatchCharacters[Math.floor(Math.random() * overwatchCharacters.length)];
+
+// function for picking a random word
+pickAWord();
+function pickAWord() {
+	$('.secretClass').text(randomWord);
+};
+
+for (var i = 0; i < randomWord.length; i++) {
+	$('#secret').text((underscore += '_ '));
+}
+
+});	
